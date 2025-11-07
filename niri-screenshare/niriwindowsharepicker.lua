3487fe13b95d5560859af4b16e7bf29261c7d3d3
Andrej Benz <hello@benz.dev>
2025-11-07 17:11:49 +0100

    add niri-screenshare

---
+++ niri-screenshare/niriwindowsharepicker.lua
@@ -0,0 +1,35 @@
+Name = "nirisharepicker"
+NamePretty = "Niri Share-Picker"
+FixedOrder = true
+Icon = "view-restore"
+Actions = {
+    share = "%VALUE%",
+}
+
+function GetEntries()
+    local entries = {}
+    local handle = io.popen("niri msg -j windows")
+    local json_output = handle:read("*a")
+    handle:close()
+
+    local data = jsonDecode(json_output)
+
+    table.insert(entries, {
+        Text = "Focused Window",
+        Value = "niri msg action set-dynamic-cast-window",
+    })
+
+    table.insert(entries, {
+        Text = "Monitor",
+        Value = "niri msg action set-dynamic-cast-monitor",
+    })
+
+    for _, window in ipairs(data) do
+        table.insert(entries, {
+            Text = window.title,
+            Value = "niri msg action set-dynamic-cast-window --id " .. tostring(window.id),
+        })
+    end
+
+    return entries
+end
